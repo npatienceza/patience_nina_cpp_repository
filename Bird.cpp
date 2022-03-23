6f04fd592e3f67cc6b3f46679573ce8a9feb596e
Nina <npatience@noctrl.edu>
2022-03-24 00:13:15 +0200

    Create Bird.h, Bird.cpp, patience_nina_lab10_main.cpp, CMakeLists.txt files

---
+++ Bird.cpp
@@ -0,0 +1,5 @@
+//Nina Patience
+//CSCE 306; Spring 2022
+//Bird class file Bird.cpp
+
+#include "Bird.h"
---
+++ patience_nina_lab10_main.cpp
@@ -0,0 +1,21 @@
+//Nina Patience
+//CSCE 306; Spring 2022
+//Lab assignment 10
+
+//main driver file patience_nina_lab10_main.cpp
+
+
+#include <iostream>
+#include <string>
+#include "Bird.h"
+
+using namespace std;
+
+int main(){
+    cout<<"Welcome to the program Tweet tweet <hello> :)"<<endl;
+
+    return 0;
+}
+
+
+
